68293b0c39a6c96e16c39ec63f5edd91bf797860
Nps9 <novi_purnamasari@gmail.com>
2025-06-05 14:13:47 +0700

    mengganti ceklis saya menjadi ceklis saja

--- app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.projectakhir_sicdx_2025
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,26 +13,5 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val editText = findViewById<EditText>(R.id.editText)
-        val imageButton = findViewById<ImageButton>(R.id.imageButton)
-        val checkBox = findViewById<CheckBox>(R.id.main_btn_checkBox)
-        val button = findViewById<Button>(R.id.main_btn_submit)
-
-        imageButton.setOnClickListener {
-            editText.setText("")
-            Toast.makeText(this, "Input direset", Toast.LENGTH_SHORT).show()
-        }
-
-        button.setOnClickListener {
-            val nama = editText.text.toString()
-            val isChecked = checkBox.isChecked
-
-            if (nama.isEmpty()) {
-                Toast.makeText(this, "Masukan nama dulu ya!", Toast.LENGTH_SHORT).show()
-            } else {
-                val status = if (isChecked) "Sudah diceklis" else "Belum diceklis"
-                Toast.makeText(this, "Halo $nama, checkbox: $status", Toast.LENGTH_LONG).show()
-            }
-        }
     }
 }
