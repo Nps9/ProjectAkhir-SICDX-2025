0b7b8cbfdb44ed0b7463df45373756c9c270f2aa
sintiaaryni <sintiaaryni@gmail.com>
2025-06-10 09:48:54 +0700

    Yang kamu tambahkan dalam kode ini adalah interaksi tombol dan logika validasi input yang ditampilkan melalui Toast.

--- app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
@@ -2,19 +2,28 @@ package com.example.projectakhir_sicdx_2025
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val editTextNama = findViewById<EditText>(R.id.editTextNama)
-        val nama = editTextNama.text.toString()
 
+        val editTextNama = findViewById<EditText>(R.id.editTextNama)
+        val btnSubmit = findViewById<Button>(R.id.main_btn_submit)
+
+        btnSubmit.setOnClickListener {
+            val nama = editTextNama.text.toString()
+
+            if (nama.isNotEmpty()) {
+                Toast.makeText(this, "Halo, $nama!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Silakan isi nama terlebih dahulu", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
