8dd1171e156141236ad0dba1e270fd09c1c6a3a3
sintiaaryni <sintiaaryni@gmail.com>
2025-06-10 23:47:04 +0700

    menambahkan aksi saat tombol gambar (ImageButton) diklik.

--- app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val btnSubmit = findViewById<Button>(R.id.main_btn_submit)
         val checkBox = findViewById<CheckBox>(R.id.main_btn_checkBox)
         val textHasil = findViewById<TextView>(R.id.textHasil)
+        val imageButton = findViewById<ImageButton>(R.id.imageButton)
 
         btnSubmit.setOnClickListener {
             val nama = editTextNama.text.toString()
@@ -33,7 +35,11 @@ class MainActivity : AppCompatActivity() {
             } else {
                 val pesan = "Halo, $nama! Selamat datang di Aplikasi Pariwisata Lampung."
                 textHasil.text = pesan
+                sudahSubmit = true
             }
+        }
+        imageButton.setOnClickListener {
+
         }
     }
 }
