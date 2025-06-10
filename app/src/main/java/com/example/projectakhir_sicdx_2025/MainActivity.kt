499aa9c43deb5b61eaf7918298d17cdcd73edcd3
sintiaaryni <sintiaaryni@gmail.com>
2025-06-10 09:41:52 +0700

    Saya tambahkan android:id="@+id/editTextNama" di EditText, supaya bisa diakses dari MainActivity.kt.

--- app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val editTextNama = findViewById<EditText>(R.id.editTextNama)
+        val nama = editTextNama.text.toString()
 
     }
 }
