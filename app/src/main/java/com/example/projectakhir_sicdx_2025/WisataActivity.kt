851cb27eda145b33b88ad5fbd5521f5d79aaad59
nps9 <novi_purnamasari@teknokrat.ac.id>
2025-06-11 22:44:39 +0700

    Menambahkan inisialisasi TextView dengan findViewById untuk mengakses komponen UI textViewWisata.

--- app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
@@ -1,11 +1,16 @@
 package com.example.projectakhir_sicdx_2025
 
+import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class WisataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wisata)
+
+        val textView: TextView = findViewById(R.id.textViewWisata)
+
 
 
     }
