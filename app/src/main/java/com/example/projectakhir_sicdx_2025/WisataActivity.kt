ff6815a8830d44cf5c8983d778839c883afcc25c
nps9 <novi_purnamasari@teknokrat.ac.id>
2025-06-11 22:36:52 +0700

    membuat sebuah Activity baru bernama WisataActivity yang mewarisi (extends) AppCompatActivity.

--- app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
@@ -1,9 +1,8 @@
 package com.example.projectakhir_sicdx_2025
 
-import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+class WisataActivity : AppCompatActivity() {
 
-class WisataActivity {
+
 }
