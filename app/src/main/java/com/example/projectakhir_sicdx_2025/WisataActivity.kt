c2c61100fdd1c2e9bdd41e9701d4e7b9815f0f3f
nps9 <novi_purnamasari@teknokrat.ac.id>
2025-06-11 22:39:31 +0700

    Menambahkan class WisataActivity yang mewarisi AppCompatActivity. Mengimplementasikan method onCreate() dan mengatur layout awal dengan activity_wisata.xml.

--- app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
@@ -3,6 +3,7 @@ package com.example.projectakhir_sicdx_2025
 import androidx.appcompat.app.AppCompatActivity
 
 class WisataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
 
 }
