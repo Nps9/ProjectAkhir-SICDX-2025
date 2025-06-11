02c0811de1fca1e60227b65a855347601cd20559
nps9 <novi_purnamasari@teknokrat.ac.id>
2025-06-11 22:41:43 +0700

    Menambahkan setContentView(R.layout.activity_wisata) di method onCreate() untuk menampilkan UI dari file layout activity_wisata.xml saat Activity dijalankan.

--- app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/WisataActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 class WisataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wisata)
+
 
     }
 
