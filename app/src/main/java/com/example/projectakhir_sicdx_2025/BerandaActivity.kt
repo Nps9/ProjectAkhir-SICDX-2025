21f4a873f6df62a481836a6b12b01fbfe13b9452
sintiaaryni <sintiaaryni@gmail.com>
2025-06-14 08:13:35 +0700

    Menambahkan activity BerandaActivity dan layout activity_beranda.xml

--- app/src/main/java/com/example/projectakhir_sicdx_2025/BerandaActivity.kt
+++ app/src/main/java/com/example/projectakhir_sicdx_2025/BerandaActivity.kt
@@ -1,2 +1,9 @@
 package com.example.projectakhir_sicdx_2025
 
+import androidx.appcompat.app.AppCompatActivity
+
+class BerandaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beranda)
+}
